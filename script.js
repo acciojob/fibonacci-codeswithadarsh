074d60b9c452d3f0c0ec6c343413eef969aec4ad
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-09-09 09:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+   let i=0;
+	let sum=0;
+	let j=1;
+	if(num == 1)
+	{
+		return 0;
+	}else if(num == 2)
+	{
+		return 1;
+	}
+    while (num-- > 2) {
+		sum= i+j;
+		i=j;
+    	j=sum;
+    }
+	return sum;
 }
 
 module.exports = fibonacci;
